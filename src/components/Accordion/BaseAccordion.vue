abaec548dfc0747d8d5a49bf37c563c00a101025
aaronjhlee <58192516+aaronjhlee@users.noreply.github.com>
2020-06-26 16:31:21 -0400

    Adding elements into accordion

--- src/components/Accordion/BaseAccordion.vue
+++ src/components/Accordion/BaseAccordion.vue
@@ -27,7 +27,7 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 :root {
   --accordioncolorcolor: #00ADEE;
 }
@@ -40,23 +40,184 @@ export default {
   display: flex;
   cursor: pointer;
   margin: 0;
-  padding: 40px 40px;
+  padding: 40px 60px;
   display: flex;
   justify-content: space-between;
-  color: white;
 }
 .plus,
 .minus {
-  font-size: 1.4rem;
+  font-size: 1.6rem;
+  color: white;
 }
 .title {
   color: white;
-  font-size: 1.2rem;
+  font-size: 1.3rem;
 }
 .content {
   text-align: left;
   width: 100%;
-  background: white;
+  background: #f3f3f3;
   padding: 40px 40px;
+  .wrapper {
+    width: 90%;
+    background: #f3f3f3;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    padding: 50px 0;
+  }
+  img {
+    padding-right: 90px;
+  }
+  .text {
+    color: black;
+    h2 {
+      font-size: 3rem;
+      padding-bottom: 20px;
+    }
+    p {
+      font-family: "Arial";
+      font-size: 1.3rem;
+      line-height: 1.5;
+    }
+  }
+  .about-us {
+    img {
+      width: 100%;
+      max-width: 400px;
+      min-width: 350px;
+    }
+  }
+
+// EVENTS STYLES
+  .events {
+    display: flex;
+    justify-content: space-around;
+  } 
+  .event {
+    cursor: pointer;
+    text-decoration: none;
+    background: white;
+    width: 350px;
+    height: 350px;
+    overflow: hidden;
+    box-shadow: 0 2px 5px 0 hsla(0,0%,65.9%,.5);
+    .image-container {
+      height: 65%;
+    }
+    img {
+      display: block;
+      height: 100%;
+      padding: 0;
+    }
+    h3 {
+      font-size: 1.3rem;
+      padding: 5px 5px;
+      color: black;
+    }
+    p {
+      font-size: 0.8rem;
+      font-family: "Arial";
+      padding: 2px 5px;
+      color: black;
+      line-height: 1.2;
+    }
+    &:hover {
+      opacity: 0.6;
+    }
+  }
+  .event-0 {
+    .image-container {
+      height: 60%;
+      width: 110%;
+    }
+    img {
+      position: relative;
+      bottom: 2px;
+    }
+  }
+  .event-1 {
+    .image-container {
+      background: #20c071;
+    }
+    img {
+      margin: 0 auto;
+    }
+  }
+
+  .vision,
+  .statement {
+    padding: 100px 0;
+  }
+  blockquote {
+    p {
+      // display: inline;
+      position: relative;
+      color: black;
+      font-size: 1.5rem;
+      text-align: center;
+    }
+    p:after, p:before {
+      color: rgba(122, 122, 122, 0.199);
+      font-size: 11rem;                                      
+      line-height: 0.8;
+      position: absolute;
+    } 
+
+    p:after {                                                       
+      content: '”';
+      display: block;
+      right: -10px;
+      top: 30px;
+    }
+
+    p:before {                                     
+      content: '“';
+      display: block;
+      left: -30px;
+      top: -40px;
+    }
+
+  }
+
+  .partners {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    .partner {
+      flex: 0 0 300px;
+      img {
+        width: 100%;
+        padding-right: 100px;
+      }
+    }
+    .kibo {
+      flex: 0 0 180px;
+    }
+  }
+  .connect {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    h2 {
+      font-size: 3rem;
+    }
+    p {
+      font-size: 1.5rem;
+      font-family: "Arial";
+      padding: 25px 0 35px;
+      line-height: 1.3;
+    }
+    .button {
+      padding: 20px 30px;
+      background: black;
+      color: white;
+      text-transform: uppercase;
+      font-weight: bold;
+      font-size: 1.2rem;
+      border-radius: 5px;
+      text-decoration: none;
+    }
+  }
 }
 </style>
--- src/components/Accordion/index.vue
+++ src/components/Accordion/index.vue
@@ -1,12 +1,48 @@
 <template>
-  <div>
-    <div class="accordion" v-for="accordion in accordions" :key="accordion.title">
+  <div class="grey-background">
+    <div class="accordion" v-for="(accordion, index) in accordions" :key="accordion.title">
       <BaseAccordion :color="accordion.color">
         <template v-slot:title>{{ accordion.title }}</template>
 
         <template v-slot:content>
-          <img src="" alt="">
-          <div>{{ accordion.text }}</div>
+          <div v-if="index === 0" class="wrapper about-us">
+            <img v-if="accordion.image" :src="accordion.image" alt="">
+            <div class="text">
+              <h2 v-if="accordion.image">{{ accordion.title }}</h2>
+              <p>{{ accordion.text }}</p>
+            </div>
+          </div>
+
+          <div v-if="index === 1" class="wrapper events">
+            <a v-for="(event, index) in accordion.events" :key="event.title" class="event" :class="'event' + '-' + index" :href="event.link" target="_blank">
+              <div class="image-container">
+                <img :src="event.image" alt="">
+              </div>
+              <div>
+                <h3>{{ event.title }}</h3>
+                <p>{{ event.description }}</p>
+              </div>
+            </a>
+          </div>
+
+          <div v-if="index === 2 || index === 3" class="wrapper vision statement">
+            <blockquote>
+              <p>{{ accordion.text }}</p>
+            </blockquote>
+          </div>
+
+          <div v-if="index === 4" class="wrapper partners">
+            <div v-for="(partner, index) in accordion.partners" :key="partner + index" :class="partner.title" class="partner">
+              <img :src="partner.logo" alt="">
+            </div>
+          </div>
+
+          <div v-if="index === 5" class="wrapper connect">
+            <h2>{{ accordion.header }}</h2>
+            <p>{{ accordion.description }}</p>
+            <a class="button" href="mailto:queerean.voices@gmail.com">Get In Touch</a>
+          </div>
+
         </template>
       </BaseAccordion>
     </div>
@@ -26,32 +62,66 @@ export default {
       accordions: [
         {
           title: "About Us",
-          text: "All about Vue",
-          color: "#E82828"
+          text: "Queerean Voices is a grassroots community group that is dedicated to advancing the health and well-being of LGBTQ+ identifying Koreans in Toronto. Our membership includes LGBTQ+ Koreans and allies.",
+          color: "#E82828",
+          image: require("@/assets/about-us-image.svg")
         },
         {
           title: "Upcoming Events",
-          text: "All about Nuxt",
-          color: "#FFA207"
+          text: "test",
+          color: "#FFA207",
+          events: [
+            {
+              title: "VOICES 2.0 (Postponed)",
+              image: require("@/assets/event-voices.jpg"),
+              description: "Are you LGBTQ+? Korean living in the GTA? Out – or not? Curious, confused? Want to be a supportive ally? Join us for VOICES of Korean LGBTQ+ and allies in the GTA. Come hear their brave stories and experiences. Share yours too. All are welcome."
+            },
+            {
+              title: "Funding For Not-For-Profit",
+              image: require("@/assets/event-gofundme.jpg"),
+              description: "Currently we are in the process of registering Queerean Voices as a non-profit organization with the Canadian Government to continue our evolving work to support Queer Koreans in the GTA. We hope you consider contributing! Thank you!",
+              link: "https://ca.gofundme.com/f/queereanvoices-not-for-profit"
+            }
+          ]
         },
         {
-          title: "Partners",
-          text: "All about webpack",
+          title: "Our Vision",
+          text: "We envision a Korean-Canadian identity that welcomes, celebrates and advocates for LGBTQ+ diversity and seeks to foster health and well-being within our intergenerational and diverse community in Canada.",
           color: "#0F9510"
         },
         {
-          title: "Resources",
-          text: "All about webpack",
+          title: "Our Mission Statement",
+          text: "To create opportunities for community-building and leadership to support Korean LGBTQ+, their families and their allies in Canada.",
           color: "#1EB7BF"
         },
         {
-          title: "Our Mission Statement",
-          text: "All about webpack",
+          title: "Partners",
+          partners: [
+            {
+              logo: require("@/assets/partner-kcsf.png"),
+              title: 'kcsf'
+            },
+            {
+              logo: require("@/assets/partner-kpwa.png"),
+              title: 'kpwa'
+
+            },
+            {
+              logo: require("@/assets/partner-pat.png"),
+              title: 'pat'
+            },
+            {
+              logo: require("@/assets/partner-kibo.png"),
+              title: 'kibo'
+
+            }
+          ],
           color: "#1E6CBF"
         },
-         {
+        {
           title: "Connect",
-          text: "All about webpack",
+          header: "We'd love to hear from you",
+          description: "Whether you have a question about membership, events, partnerships, or anything else, we'd be happy to answer all your questions.",
           color: "#761EBF"
         }
       ]
@@ -63,6 +133,9 @@ export default {
 <style lang="scss" scoped>
 .accordion {
   margin-bottom: 3px;
+}
+.grey-background {
+  background: #f3f3f3;
 }
 
 </style>
